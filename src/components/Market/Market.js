813d5c22bee587906da41afe4075ec3d512dbd67
iBW <84953059+iceBlockWare@users.noreply.github.com>
2022-03-21 22:46:02 -0600

    Update DApp Backend (#2)
    * Update store.js (linting and syntax)
    
    This is the revised edition of store.js
    
    * Update Header.js
    
    Business Logic integration to web3-enabled button.
    
    * Update Collections.js
    
    It still needs an HTML / CSS tweaking in order to display NFTs as we want them to be presented: NFT/COLLECTION/SIDE/TIER/SERIES/EDITION/POWER (which corresponds to an infrastructural design, abiding to the WhitePaper, Strategic Planning, and Branding), so the UX is always at its best, within our current possibilities, meaning that navigation over our website should always be intuitive, easy, fun and entertaining. Displaying a whole bunch of images (which are also distorted by some styling we need to correct). Overall I think it's looking fine.
    
    * Update Login.js
    
    Linting and Syntax fixes
    
    * Update Market.js
    
    Linting & Syntax
    
    * Update Menu.js
    
    * Update Collections.js
    
    * Update store.js
    
    Business Logic review and complying
    
    * Update store.js
    
    Business Logic integration & linting

--- src/components/Market/Market.js
+++ src/components/Market/Market.js
@@ -1,171 +1,163 @@
-import React from "react";
-import style from "./Market.css";
-import appStyle from "../../App.module.css";
-import Footer from "../Footer/Footer";
-import Header from "../Header/Header";
-import Store from "../../store/store";
+import React from 'react';
+import style from './Market.css';
+import appStyle from '../../App.module.css';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import Store from '../../store/store';
 
 const store = Store.store;
-const emitter = Store.emitter
+const emitter = Store.emitter;
 
 class Market extends React.Component {
   constructor(props) {
     super(props);
-    const accountAddress = store.getStore("accountAddress");
-    const haveNFT = store.getStore("haveNFT");
-    const isInWar = store.getStore("isInWar");
-    const dwarf = store.getStore("dwarf");
-    const opened = store.getStore("opened");
-    const NFTs = store.getStore("NFTs");
-
+    const accountAddress = store.getStore('accountAddress');
+    const haveNFT = store.getStore('haveNFT');
+    const isInWar = store.getStore('isInWar');
+    const dwarf = store.getStore('dwarf');
+    const opened = store.getStore('opened');
+    const NFTs = store.getStore('NFTs');
     this.state = {
       accountAddress: accountAddress,
-      haveNFT:haveNFT,
-      isInWar:isInWar,
-      dwarf:dwarf,
-      opened:opened,
-      NFTs:NFTs,
-    };
+      haveNFT: haveNFT,
+      isInWar: isInWar,
+      dwarf: dwarf,
+      opened: opened,
+      NFTs: NFTs,
+    }
     this.balances = this.balances.bind(this);
     this.nbalances = this.nbalances.bind(this);
     this.openclose = this.openclose.bind(this);
   }
-  
     
   balances() {
-    const accountAddress = store.getStore("accountAddress");
-    const haveNFT = store.getStore("haveNFT");
-    const isInWar = store.getStore("isInWar");
-    const dwarf = store.getStore("dwarf");
-    const opened = store.getStore("opened");
-    const NFTs = store.getStore("NFTs");
+    const accountAddress = store.getStore('accountAddress');
+    const haveNFT = store.getStore('haveNFT');
+    const isInWar = store.getStore('isInWar');
+    const dwarf = store.getStore('dwarf');
+    const opened = store.getStore('opened');
+    const NFTs = store.getStore('NFTs');
     this.setState({
-        accountAddress:accountAddress,
-        haveNFT:haveNFT,
-        isInWar:isInWar,
-        dwarf:dwarf,
-        opened:opened,
-        NFTs:NFTs,
-    })
-    console.log("got new balances")
+        accountAddress: accountAddress,
+        haveNFT: haveNFT,
+        isInWar: isInWar,
+        dwarf: dwarf,
+        opened: opened,
+        NFTs: NFTs,
+    });
+    console.log('got new balances');
   }
   
   async nbalances() {
     await new Promise(resolve => setTimeout(resolve, 1000));
-    const NFTs = store.getStore("NFTs");
+    const NFTs = store.getStore('NFTs');
     await this.setState({
-        NFTs:NFTs,
-    })
-    console.log("got new NFT balances")
+      NFTs: NFTs,
+    });
+    console.log('got new NFT balances');
   }
   
   async stakeDwarf() {
-      store.setReady(true);
-      const accountAddress =  store.getStore("accountAddress");
-      const dwarfAddress =  store.getStore("dwarfAddress");
-      const dwarfABI =  store.getStore("dwarfABI");
-      const web3 =  store.getStore("web3");
-      const dwarfContract = new web3.eth.Contract(dwarfABI, dwarfAddress);
-
-      const erc20ABI =  store.getStore("erc20ABI");
-      const dwarf20Address =  store.getStore("dwarf20Address");
-
-      let dwarf20Contract = new web3.eth.Contract(erc20ABI, dwarf20Address)
-
-      var allowance = await dwarf20Contract.methods.allowance(accountAddress, dwarfAddress).call({ from: accountAddress, })
-      const ethAllowance = parseFloat(allowance)/10**18;
-      if(parseFloat(ethAllowance) < 3000) {
-          await dwarf20Contract.methods.approve(dwarfAddress, web3.utils.toWei("999999999999999", "ether")).send({ from: accountAddress, })
-      }
-
+    store.setReady(true);
+    const accountAddress = store.getStore('accountAddress');
+    const dwarfAddress = store.getStore('dwarfAddress');
+    const dwarfABI = store.getStore('dwarfABI');
+    const web3 = store.getStore('web3');
+    const dwarfContract = new web3.eth.Contract(dwarfABI, dwarfAddress);
+    const erc20ABI = store.getStore('erc20ABI');
+    const dwarf20Address = store.getStore('dwarf20Address');
+    let dwarf20Contract = new web3.eth.Contract(erc20ABI, dwarf20Address);
+    var allowance = await dwarf20Contract.methods.allowance(accountAddress, dwarfAddress).call({ from: accountAddress, });
+    const ethAllowance = parseFloat(allowance)/10**18;
+    if(parseFloat(ethAllowance) < 3000) {
+      await dwarf20Contract.methods.approve(dwarfAddress, web3.utils.toWei('999999999999999', 'ether')).send({ from: accountAddress, })
+    }
       try {
         var result = await dwarfContract.methods.openmarket().send({ from: accountAddress, });
       } catch(error) {
         store.setReady(false);
       }
-      store.checkMarket();
-      store.setReady(false);
+    store.checkMarket();
+    store.setReady(false);
   }
 
   async claimDwarf() {
-      store.setReady(true);
-      const accountAddress =  store.getStore("accountAddress");
-      const dwarfAddress =  store.getStore("dwarfAddress");
-      const dwarfABI =  store.getStore("dwarfABI");
-      const web3 =  store.getStore("web3");
-      const dwarfContract = new web3.eth.Contract(dwarfABI, dwarfAddress);
-      try {
-        var result = await dwarfContract.methods.closemarket().send({ from: accountAddress, });
-      } catch(error) {
-        store.setReady(false);
-      }
-      store.checkMarket();
+    store.setReady(true);
+    const accountAddress = store.getStore('accountAddress');
+    const dwarfAddress = store.getStore('dwarfAddress');
+    const dwarfABI = store.getStore('dwarfABI');
+    const web3 = store.getStore('web3');
+    const dwarfContract = new web3.eth.Contract(dwarfABI, dwarfAddress);
+    try {
+      var result = await dwarfContract.methods.closemarket().send({ from: accountAddress, });
+    } catch(error) {
       store.setReady(false);
+    }
+    store.checkMarket();
+    store.setReady(false);
   }
   
   async buyJediNFT(id) {
-      store.setReady(true);
-      const accountAddress =  store.getStore("accountAddress");
-      const dwarfAddress =  store.getStore("dwarfAddress");
-      const dwarfABI =  store.getStore("dwarfABI");
-      const web3 =  store.getStore("web3");
-      const dwarfContract = new web3.eth.Contract(dwarfABI, dwarfAddress);
-      try {
-        var result = await dwarfContract.methods.buyjedi(id).send({ from: accountAddress, });
-        console.log(result);
-      } catch(error) {
-        store.setReady(false);
-        console.log(error);
-      }
+    store.setReady(true);
+    const accountAddress = store.getStore('accountAddress');
+    const dwarfAddress = store.getStore('dwarfAddress');
+    const dwarfABI = store.getStore('dwarfABI');
+    const web3 = store.getStore('web3');
+    const dwarfContract = new web3.eth.Contract(dwarfABI, dwarfAddress);
+    try {
+      var result = await dwarfContract.methods.buyjedi(id).send({ from: accountAddress, });
+      console.log(result);
+    } catch(error) {
       store.setReady(false);
-      store.getBalances();
+      console.log(error);
+    }
+    store.setReady(false);
+    store.getBalances();
   }
 
   async buyDarthNFT(id) {
-      store.setReady(true);
-      const accountAddress =  store.getStore("accountAddress");
-      const dwarfAddress =  store.getStore("dwarfAddress");
-      const dwarfABI =  store.getStore("dwarfABI");
-      const web3 =  store.getStore("web3");
-      const dwarfContract = new web3.eth.Contract(dwarfABI, dwarfAddress);
-      try {
-        await dwarfContract.methods.buydarth(id).send({ from: accountAddress, })
-          .on('confirmation', function(confirmationNumber, receipt){
-            console.log(confirmationNumber, receipt);
-            if(confirmationNumber == 1) {
-              store.getBalances();
-            }
-          })
-        console.log("done");
-      } catch(error) {
-        store.setReady(false);
-        console.log(error);
-      }
+    store.setReady(true);
+    const accountAddress = store.getStore('accountAddress');
+    const dwarfAddress = store.getStore('dwarfAddress');
+    const dwarfABI = store.getStore('dwarfABI');
+    const web3 = store.getStore('web3');
+    const dwarfContract = new web3.eth.Contract(dwarfABI, dwarfAddress);
+    try {
+      await dwarfContract.methods.buydarth(id).send({ from: accountAddress, })
+      .on('confirmation', function(confirmationNumber, receipt){
+        console.log(confirmationNumber, receipt);
+          if(confirmationNumber === 1) {
+            store.getBalances();
+          }
+      })
+      console.log('done');
+    } catch(error) {
       store.setReady(false);
-      store.getBalances();
+      console.log(error);
+    }
+    store.setReady(false);
+    store.getBalances();
   }
 
-
   componentWillMount() {
-      emitter.on('balances', this.balances);
-      emitter.on('nbalances', this.nbalances);
-      emitter.on('opened', this.openclose);
+    emitter.on('balances', this.balances);
+    emitter.on('nbalances', this.nbalances);
+    emitter.on('opened', this.openclose);
   }
   
   componentWillUnmount() {
-      emitter.on('balances', this.balances);
-      emitter.on('nbalances', this.balances);
-      emitter.on('opened', this.openclose);
+    emitter.on('balances', this.balances);
+    emitter.on('nbalances', this.balances);
+    emitter.on('opened', this.openclose);
   }
   
   openclose(opened) {
     this.setState({
-        opened:opened,
+      opened:opened,
     })
   };
 
   renderJediNFTs = (NFTs) => {
-
     return NFTs.filter((NFT) => {
       if(NFT.side === 'jedi') {
         return true
@@ -179,7 +171,6 @@ class Market extends React.Component {
   
 
   renderDarthNFTs = (NFTs) => {
-
     return NFTs.filter((NFT) => {
       if(NFT.side === 'darth') {
         return true
@@ -193,29 +184,29 @@ class Market extends React.Component {
 
   
   renderNFT = (NFT) => {
-    var goto =  () => this.buyJediNFT(NFT.id);
+    var goto = () => this.buyJediNFT(NFT.id);
     if(NFT.side === 'darth') {
       goto = () => this.buyDarthNFT(NFT.id);
     }
     return (
-            <div className={appStyle.nfblock}>
-              <div className={appStyle.nfblockPower}>
-                <span>Damage: {NFT.pd}</span>
-                <span>Kinetics: {NFT.pk}</span>
-                <span>Speed: {NFT.ps}</span>
-                <span>Conversion: {NFT.pc}</span>
-                <span>Healing: {NFT.ph}</span>
-                <span>{NFT.suply}/{NFT.total}</span>
-              </div>
-              <div className={appStyle.nfblockMine}>
-                <span>{NFT.amount}</span>
-              </div>
-              <img src={NFT.logo} alt="" />
-              <p>{NFT.title}</p>
-              <p className={NFT.side}>{NFT.price} <br />DWARF</p>
+      <div className={appStyle.nfblock}>
+        <div className={appStyle.nfblockPower}>
+          <span>Damage: {NFT.pd}</span>
+          <span>Kinetics: {NFT.pk}</span>
+          <span>Speed: {NFT.ps}</span>
+          <span>Conversion: {NFT.pc}</span>
+          <span>Healing: {NFT.ph}</span>
+          <span>{NFT.suply}/{NFT.total}</span>
+        </div>
+        <div className={appStyle.nfblockMine}>
+          <span>{NFT.amount}</span>
+        </div>
+        <img src={NFT.logo} alt='' />
+        <p>{NFT.title}</p>
+        <p className={NFT.side}>{NFT.price} <br />DWARF</p>
 
-              <button onClick={goto}>Buy</button>
-            </div>
+        <button onClick={goto}>Buy</button>
+      </div>
     )
   }
   
@@ -225,49 +216,47 @@ class Market extends React.Component {
       <div className={style.Sword}>
         <Header />
         <div className={appStyle.container}>
-{(() => {
-    if (opened) {
-      return (
-        <div className={appStyle.flexcol}>
-          <div className={appStyle.flexrow}>
-            <div className={style.text}>
-            {
-              this.renderJediNFTs(NFTs)
+          {(() => {
+            if (opened) {
+              return (
+                <div className={appStyle.flexcol}>
+                  <div className={appStyle.flexrow}>
+                    <div className={style.text}>
+                      {
+                        this.renderJediNFTs(NFTs)
+                      }
+                    </div>
+                    <div className={style.text}>
+                      {
+                        this.renderDarthNFTs(NFTs)
+                      }
+                    </div>
+                  </div>
+                  <div className={style.textcenter}>
+                    <div className={appStyle.nfblock}>
+                      <img src='img/market.png' alt='NFT Marketplace' />
+                      <p>{dwarf} <br />DWARF</p>
+                      <button onClick={this.claimDwarf}>Leave Warzone <br />Marketplace</button>
+                    </div>
+                  </div>
+                </div>
+              )
+            } else {
+              return (
+                <div className={style.textcenter}>
+                  <div className={appStyle.nfblock}>
+                    <img src='img/market.png' alt='NFT Marketplace' />
+                    <p>{dwarf} <br />DWARF</p>
+                    <button disabled={dwarf === 0} onClick={this.stakeDwarf}>Enter Warzone <br />Marketplace</button>
+                  </div>
+                </div>
+              )
             }
-            </div>
-            <div className={style.text}>
-            {
-              this.renderDarthNFTs(NFTs)
-            }
-            </div>
-          </div>
-          <div className={style.textcenter}>
-            <div className={appStyle.nfblock}>
-              <img src="img/market.png" alt="NFT Marketplace" />
-              <p>{dwarf} <br />DWARF</p>
-
-              <button onClick={this.claimDwarf}>Close NFT Marketplace</button>
-            </div>
-          </div>
-        </div>
-      )
-    } else {
-      return (
-          <div className={style.textcenter}>
-            <div className={appStyle.nfblock}>
-              <img src="img/market.png" alt="NFT Marketplace" />
-              <p>{dwarf} <br />DWARF</p>
-
-              <button disabled={dwarf==0} onClick={this.stakeDwarf}>Open NFT Marketplace</button>
-            </div>
-          </div>
-      )
-    }
-})()}
+          })()}
         </div>
         <Footer />
       </div>
-    );
+    )
   }
 }
 
